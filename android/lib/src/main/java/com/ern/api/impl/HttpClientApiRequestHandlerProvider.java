70f4d45506fc15bf611a28fd179f8ba5740bb9b8
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-02-05 14:38:42 -0200

    First naive implementation

--- android/lib/src/main/java/com/ern/api/impl/HttpClientApiRequestHandlerProvider.java
+++ android/lib/src/main/java/com/ern/api/impl/HttpClientApiRequestHandlerProvider.java
@@ -13,24 +13,84 @@
 
 package com.ern.api.impl;
 
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
+import com.HttpClientApi.ern.api.HttpClientApi;
+import com.HttpClientApi.ern.api.FetchData;
+import com.walmartlabs.electrode.reactnative.bridge.ElectrodeBridgeRequestHandler;
+import com.walmartlabs.electrode.reactnative.bridge.ElectrodeBridgeResponseListener;
+
+import io.reactivex.Single;
+import io.reactivex.SingleEmitter;
+import io.reactivex.SingleOnSubscribe;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+
 /**
-* This class is a generated place holder for your HttpClient implementations.!
-* Feel free to modify this class contents as needed. `ern regen-api-impl` command WILL NOT modify the content of this class.
-* Don't modify the class name as this naming convention is used for container generation.
-*/
+ * This class is a generated place holder for your HttpClient implementations.!
+ * Feel free to modify this class contents as needed. `ern regen-api-impl` command WILL NOT modify the content of this class.
+ * Don't modify the class name as this naming convention is used for container generation.
+ */
 public class HttpClientApiRequestHandlerProvider extends RequestHandlerProvider<HttpClientApiRequestHandlerProvider.HttpClientApiConfig> implements HttpClientApiRequestHandler {
+    private static final String GET = "get";
+    private static final String POST = "post";
+    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
     /**
-    * @param requestHandlerConfig : Optional config object that can be passed to an api impl provider.
-    */
+     * @param requestHandlerConfig : Optional config object that can be passed to an api impl provider.
+     */
     HttpClientApiRequestHandlerProvider(@Nullable HttpClientApiRequestHandlerProvider.HttpClientApiConfig requestHandlerConfig) {
         super(requestHandlerConfig);
     }
 
     @Override
     public void registerFetchRequestHandler() {
+        HttpClientApi.requests().registerFetchRequestHandler(new ElectrodeBridgeRequestHandler<FetchData, com.HttpClientApi.ern.model.Response>() {
+            @Override
+            public void onRequest(@Nullable FetchData payload, @NonNull final ElectrodeBridgeResponseListener<com.HttpClientApi.ern.model.Response> responseListener) {
+                assert payload != null;
+
+                final OkHttpClient client = new OkHttpClient();
+
+                Request.Builder builder = new Request.Builder()
+                        .url(payload.geturl());
+                switch (payload.getmethod().toLowerCase()) {
+                    case HttpClientApiRequestHandlerProvider.GET:
+                        builder = builder.get();
+                        break;
+                    case HttpClientApiRequestHandlerProvider.POST:
+                        RequestBody body = RequestBody.create(JSON, "");
+                        builder = builder.post(body);
+                }
+
+                final Request request = builder.build();
+
+                Single.create(new SingleOnSubscribe<String>() {
+                    @Override
+                    public void subscribe(SingleEmitter<String> emitter) throws Exception {
+                        Response response = client.newCall(request).execute();
+                        emitter.onSuccess(response.body().string());
+                    }
+                }).subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(new Consumer<String>() {
+                            @Override
+                            public void accept(String s) throws Exception {
+                                com.HttpClientApi.ern.model.Response response = new com.HttpClientApi.ern.model.Response.Builder().body(s).headers("headers...").status(200).build();
+                                responseListener.onSuccess(response);
+                            }
+                        });
+            }
+        });
+
         //TODO
     }
 
